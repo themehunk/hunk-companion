41d3cbcb4a278f31bbed213b23094ea708c6fc73
ThemeHunk <themehunk@gmail.com>
2024-08-17 18:59:47 +0530

    new shop mania templates added

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'd84291d03c79b007dbfa');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '7da23384801f27862069');
--- readme.txt
+++ readme.txt
@@ -61,6 +61,7 @@ Yes this plugin is specially make for ThemeHunk themes like Gogo. You can instal
 
 = 1.8.1 =
 * New : Theme limitation added.
+* New : Added new shop mania templates.
 
 = 1.8.0 =
 * New : New demo added in shop mania theme.
