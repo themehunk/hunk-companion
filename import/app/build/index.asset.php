321369cb6ba7e905ba2b8bf867e5443b1e978308
ThemeHunk <themehunk@gmail.com>
2024-11-25 13:12:00 +0530

    bild file upadte

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '41060df9b5c1d14b2a95');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'a86b4a9150da928a251e');
