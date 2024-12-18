5f0df9536cb462fd6604b0a2b6d1c5cbd6119646
ThemeHunk <themehunk@gmail.com>
2024-12-18 19:06:14 +0530

    update new demo

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'bad2ba57672f598a1275');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '4db3aa568925824e790e');
