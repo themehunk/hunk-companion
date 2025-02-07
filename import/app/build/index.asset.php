2989581efc308d8a30d7f42792d3e2aabaa72a2d
iheeresh <heereshd@gmail.com>
2025-02-07 13:59:57 +0530

    th-shopmania.json updated

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '30847ad97496936ce5c4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '14c01ca46f2b486acc0c');
