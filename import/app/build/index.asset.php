8e414653288feac794988cdd84b43cb8db7e7448
iheeresh <heereshd@gmail.com>
2025-02-27 13:16:43 +0530

    th-shop-mania.json updated

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '14c01ca46f2b486acc0c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '3d8d4f09126497b6654d');
