19ae0a730fa1732d6c164543887d9c66ee4a947d
ThemeHunk <themehunk@gmail.com>
2025-06-21 17:42:55 +0530

    demo upper

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '915506095a9765172f4e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'eb88f3eddf70416d6bcf');
