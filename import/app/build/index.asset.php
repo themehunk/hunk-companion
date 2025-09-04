c61109032070be1c68778d8de610f42a7148314a
iheeresh <heereshd@gmail.com>
2025-09-04 12:21:16 +0530

    open shop thwishlist added

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'c2f18517a5a1bc4f0742');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'ac46a970e763c69b5b9c');
