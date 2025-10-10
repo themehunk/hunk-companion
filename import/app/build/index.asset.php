27d02864e5e0deae3f38ee238c105acb028e5da0
iheeresh <heereshd@gmail.com>
2025-10-10 14:36:17 +0530

    demo converted to paid

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '9416c47990745712fe2a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'd6e17118901845d8753c');
