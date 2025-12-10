d29cf5c2c057ff013716ea1f3d70285fc8979fda
iheeresh <heereshd@gmail.com>
2025-12-10 12:12:28 +0530

    demo thumbnail updated

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '3ef3701db492d455d2b8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '91d7fce153aedcc3ea45');
