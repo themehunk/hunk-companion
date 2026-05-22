d4fdc3bd107b7d42a587116c91fbcb650e0ee520
iheeresh <heereshd@gmail.com>
2026-05-22 18:29:22 +0530

    plugins.json updated

--- import/app/build/index.asset.php
+++ import/app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '0c425a6729bc910ffee1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'b829779815898cd1b996');
